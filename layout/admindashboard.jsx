ca8a689076c3d786075bacede170dcf51a73e095
John Cletus <jhornelcle@gmail.com>
2023-02-24 08:02:52 +0100

    admin files

---
+++ layout/admindashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./admindashboard.module.css"
+
+function AdminDashboard(){
+    return(
+        <h1>testing the mic</h1>
+    )
+    
+}
+
+
+export default AdminDashboard;
